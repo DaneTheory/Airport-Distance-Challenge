57524cf20fcba1be23a6ea8de215080d258b3ece
Branden Dane <danetheory@gmail.com>
2017-07-05 12:24:59 -0400

    Initial Push
    initial push

---
+++ config/configs.js
@@ -0,0 +1,8 @@
+export const EegeoAPIKey = `1027b6f2db623ee6b48ba728288cb654`
+export const AirportAPI = {
+  key: `0343ffe274`,
+  secret: `bd64420a4fc75a7`,
+  baseUrl: `https://www.air-port-codes.com/api/v1/`,
+  autocompleteEndpoint: `autocomplete?type=a&term=`,
+  singleAirportEndpoint: `single?iata=`
+}
